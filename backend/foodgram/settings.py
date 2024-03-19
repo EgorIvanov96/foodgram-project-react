df4d27696056b79470b442bb63bede7d48c26649
EgorIvanov96 <shura.1996.ivanov@gmail.com>
2024-03-19 18:33:23 +0300

    Test16

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -88,14 +88,14 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Для локального запуска
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
@@ -104,7 +104,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
-}"""
+}
 
 
 # Password validation
---
+++ backend/reviews/management/commands/import_csv.py
@@ -0,0 +1,23 @@
+import csv
+
+from django.core.management import BaseCommand
+
+from reviews.models import Ingredient
+
+
+class Command(BaseCommand):
+    def import_ingredients(self):
+        with open(
+            '../backend/data/ingredients.csv', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                measurement_unit = row['measurement_unit']
+                Ingredient.objects.create(
+                    name=name, measurement_unit=measurement_unit
+                )
+
+    def handle(self, *args, **options):
+        self.import_ingredients()
+        self.stdout.write(self.style.SUCCESS('Data imported successfully'))
