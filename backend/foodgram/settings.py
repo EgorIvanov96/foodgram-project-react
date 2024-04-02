d4415f8cee69ee208fb5b67cdb71fcfaa0aea920
EgorIvanov96 <shura.1996.ivanov@gmail.com>
2024-04-02 17:08:26 +0300

    Test22

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', False) == "True"
-# DEBUG = True
+# DEBUG = os.getenv('DEBUG', False) == "True"
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
